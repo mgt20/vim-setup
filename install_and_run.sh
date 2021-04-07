8b67cb9d47918045fd58b630b35d91ca28ccb184
mgt20 <66187533+mgt20@users.noreply.github.com>
2021-04-06 23:50:18 -0700

    Update install_and_run.sh

--- install_and_run.sh
+++ install_and_run.sh
@@ -3,26 +3,10 @@ sudo apt-get install git -y
 sudo apt-get install neovim -y
 sudo apt-get install python3-neovim -y
 sudo apt-get install nodejs -y
+sudo apt-get install python3-pip -y
+python3 -m pip install --user --upgrade pynvim
 
 
 ~/.sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# After installing vim-plug, add these plug-ins
-
-- dracula/vim
-- neoclide/coc.nvim
-- vim-airline/vim-airline
-- preservim/nerdtree
-- raimon49/requirements.txt.vim
-
-After installing coc.nvim, run:
-- vim
-- :PlugUpgrade
-- :PlugUpdate
-- :CocInstall coc-pyright
-
-- nvim
-- :PlugUpgrade
-- :PlugUpdate
-- :CocInstall coc-pyright
