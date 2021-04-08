3c95fba8e8f88bae0f3cfb5175fe01dd46824ee7
mgt20 <66187533+mgt20@users.noreply.github.com>
2021-04-07 17:08:50 -0700

    adding init.vim template

--- install_and_run.sh
+++ install_and_run.sh
@@ -4,10 +4,10 @@ sudo apt-get install neovim -y
 sudo apt-get install python3-neovim -y
 sudo apt-get install nodejs -y
 sudo apt-get install python3-pip -y
+sudo apt install nodejs npm -y
 python3 -m pip install --user --upgrade pynvim
-curl -sL install-node.now.sh/lts | bash
-
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+cp init.vim ~/.config/nvim/init.vim
