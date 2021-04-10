19eef166471ee4614e697f393a78543e9111f728
mgt20 <66187533+mgt20@users.noreply.github.com>
2021-04-10 12:36:23 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,4 +10,4 @@ python3 -m pip install --user --upgrade pynvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-mkdir -p ~/.config/nvim && cp init.vim $_
+mkdir -p ~/.config/nvim
