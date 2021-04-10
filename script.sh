19ebec0b13f6e71ac74117b6726201465483d81d
mgt20 <66187533+mgt20@users.noreply.github.com>
2021-04-10 12:37:52 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,11 @@
 sudo apt-get update
-sudo apt-get install git -y
-sudo apt-get install neovim -y
-sudo apt-get install python3-neovim -y
-sudo apt-get install nodejs -y
-sudo apt-get install python3-pip -y
-sudo apt install nodejs npm -y
+sudo apt install \
+       git \
+       neovim \
+       python3-neovim \
+       nodejs \
+       python3-pip \
+       nodejs npm -y
 python3 -m pip install --user --upgrade pynvim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
