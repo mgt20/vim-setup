5c58fe1ba8e7521ce76e6afa2c8a26df890c3460
mgt20 <66187533+mgt20@users.noreply.github.com>
2022-01-29 10:12:10 +0000

    detecting distro

--- script.sh
+++ script.sh
@@ -1,20 +1,68 @@
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-sudo apt install \
-       git \
-       neovim \
-       python3-neovim \
-       nodejs \
-       python3-pip \
-       nodejs \
-       yarn -y
-python3 -m pip install --user --upgrade pynvim
+#/bin/bash
+# Setup VIM and VIM plugins and preferences
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+get_os() {
+        if [ -f /etc/os-release ]; then
+            # freedesktop.org and systemd
+            . /etc/os-release
+            OS=$NAME
+            VER=$VERSION_ID
+        elif type lsb_release >/dev/null 2>&1; then
+            # linuxbase.org
+            OS=$(lsb_release -si)
+            VER=$(lsb_release -sr)
+        elif [ -f /etc/lsb-release ]; then
+            # For some versions of Debian/Ubuntu without lsb_release command
+            . /etc/lsb-release
+            OS=$DISTRIB_ID
+            VER=$DISTRIB_RELEASE
+        elif [ -f /etc/debian_version ]; then
+            # Older Debian/Ubuntu/etc.
+            OS=Debian
+            VER=$(cat /etc/debian_version)
+        elif [ -f /etc/SuSe-release ]; then
+            # Older SuSE/etc.
+            ...
+        elif [ -f /etc/redhat-release ]; then
+            # Older Red Hat, CentOS, etc.
+            ...
+        else
+            # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+            OS=$(uname -s)
+            VER=$(uname -r)
+        # https://unix.stackexchange.com/a/6348
+        fi
+        }
 
-mkdir -p ~/.config/nvim
+distro_ubuntu_setup() {
+        echo "You will be prompted for your sudo credentials in order to execute the next commands"
+        sudo apt-get update
+        curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+        # https://github.com/nodesource/distributions/blob/master/README.md
+        curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+        echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+        sudo apt-get update
+        sudo apt install \
+               git \
+               neovim \
+               python3-neovim \
+               nodejs \
+               python3-pip \
+               nodejs \
+               yarn -y
+        python3 -m pip install --user --upgrade pynvim
 
-sudo apt-get install -y nodejs
+        sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+               https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+        mkdir -p ~/.config/nvim
+
+        sudo apt-get install -y nodejs
+}
+
+get_os
+if [ $NAME == "Ubuntu" ]; then
+        echo "installing stuff for Ubuntu"
+        distro_ubuntu_setup
+fi
+
