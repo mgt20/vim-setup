f6d741a6387f5ef47fd813d6e9583a0f3d809fb0
mgt20 <66187533+mgt20@users.noreply.github.com>
2022-01-30 23:26:04 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -69,10 +69,7 @@ distro_ubuntu_setup() {
         sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-        mkdir -p ~/.config/nvim
-
-        sudo apt-get install -y nodejs
-}
+        mkdir -p ~/.config/nvim}
 
 get_os
 if [ $NAME == "Ubuntu" ]; then
